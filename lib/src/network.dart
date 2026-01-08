4dee1c1303cc7c39e8fdf172c1366c2b41ef7abb
Josh Holtz <me@joshholtz.com>
2026-01-07 21:08:59 -0600

    Bump version to 0.2.5 (#16)
    Co-authored-by: joshdholtz <401294+joshdholtz@users.noreply.github.com>

--- lib/src/network.dart
+++ lib/src/network.dart
@@ -8,7 +8,7 @@ import 'types.dart';
 import 'utils.dart';
 
 /// SDK version for metrics headers
-const String sdkVersion = '0.2.4';
+const String sdkVersion = '0.2.5';
 
 /// Abstract interface for the network client.
 abstract class NetworkClient {
